a670f38b995433fa02fdad2ab901829ead4ed7c6
cuefe <2150269402@qq.com>
2024-03-25 20:24:08 +0800

    DES-demo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     ofstream encrypt_file("encrypt.txt", ios::trunc);
     encrypt_file.close();
-    std::ofstream decrypt_file("decrypt.txt", ios::trunc);
+    ofstream decrypt_file("decrypt.txt", ios::trunc);
     decrypt_file.close();
     string key = "ucalzped";
     encrypt("plain.txt", "encrypt.txt", key);
